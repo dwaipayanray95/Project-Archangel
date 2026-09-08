14707da64251f6f146decaaee3a32e1e2af97e02
dwaipayanray95 <dwaipayanray95@gmail.com>
2026-09-08 13:35:23 +0530

    fix(windows): force local CMake install prefix and bump version to 0.3.8

--- app/frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ app/frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,6 @@ import Foundation
 import flutter_secure_storage_darwin
 import local_auth_darwin
 import mobile_scanner
-import path_provider_foundation
 import url_launcher_macos
 import wireguard_flutter
 
@@ -16,7 +15,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WireguardFlutterPlugin.register(with: registry.registrar(forPlugin: "WireguardFlutterPlugin"))
 }
